b0258a99d28f8df1fda13fbbb1918459cc0e9ce2
Eugenia Toledo <m.eugenia.toledo@hotmail.com>
2023-03-21 17:16:28 -0300

    added folders of project

---
+++ javascript/index.js
@@ -0,0 +1,57 @@
+const $ = (selector) => document.querySelector(selector)
+
+// Board Image appear
+
+$("#button__image").addEventListener("click", () => {
+    $(".board__image").classList.remove("hidden")
+    if($(".board__image").style.display = "block"){
+        $(".board__text").style.display = "none"
+    }
+})
+
+// Board Image disappear
+
+$(".board__btn-exit-image").addEventListener("click", () =>{
+    $(".board__image").classList.add("hiden")
+    $(".board__image").style.display = "none"
+})
+
+//Board Text appear
+
+$("#button__text").addEventListener("click", ()=> {
+    $(".board__text").classList.remove("hidden")
+    if ($(".board__text").style.display = "block"){
+        $(".board__image").style.display = "none"
+    }
+})
+
+//Board Text desappear
+
+$(".board__btn-exit-text").addEventListener("click", () =>{
+    $(".board__text").classList.add("hiden")
+    $(".board__text").style.display = "none"
+})
+
+// Change mode light or dark
+
+const change__mode = () => {
+    const dark = $("body").getAttribute("data-theme")
+    if (dark === $("light__mode")){
+        $("#button__dark").classList.remove("hidden")
+        $("#button__light").classList.add("hidden")
+        $("body").setAttribute("data-theme", "light__mode")
+    } else {
+        $("#button__dark").classList.add("hidden")
+        $("#button__light").classList.remove("hidden")
+        $("body").removeAttribute("data-theme", "light__mode")
+    }
+}
+
+
+
+// Buttons dark and light
+
+
+$("#button__dark").addEventListener("click",change__mode)
+
+$("#button__light").addEventListener("click",change__mode)
